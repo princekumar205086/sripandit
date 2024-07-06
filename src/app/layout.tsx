e03cb588ce10be987972c9f42ac2449938596e3b
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2024-07-07 02:11:03 +0530

    whatsapp floating button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from "./(componentsUi)/navbar/page";
 import Footer from "./(componentsUi)/footer/page";
 import { ToastContainer } from 'react-toastify';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });  
 export const metadata: Metadata = {
@@ -25,6 +27,22 @@ export default function RootLayout({
         <Footer />
         <ToastContainer />
         <SpeedInsights/>
+        <Link href="https://wa.me/9471661636" target="_blank" rel="noopener noreferrer" style={{
+          position: 'fixed',
+          width: '60px',
+          height: '60px',
+          bottom: '40px',
+          right: '40px',
+          backgroundColor: '#25d366',
+          color: '#FFF',
+          borderRadius: '50px',
+          textAlign: 'center',
+          fontSize: '30px',
+          boxShadow: '2px 2px 3px #999',
+          zIndex: '100',
+        }}>
+          <FaWhatsapp style={{ marginTop: '16px', marginLeft: '15px' }} />
+        </Link>
       </body>
     </html>
   );
